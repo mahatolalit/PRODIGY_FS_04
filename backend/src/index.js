c4e2fbbe0c7c9c209d65129b5fe060980e6db908
mahatolalit <lalitmahato20051024@gmail.com>
2025-07-12 08:04:57 +0530

    feat: implement messaging routes, controller, and socket.io integration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.route.js"
-import { connectDB } from "./lib/db.js"
-import cookieParser from "cookie-parser"
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is running on port " + PORT);
